2bda4ebadb76d771b0c55c6655981704f8688936
minato aqua <homosapien@foxmail.com>
2022-02-28 18:50:15 +0800

    week1 commit

---
+++ W1HW2App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            for (int i = 0; i < week.Length; i++)
+            {
+                Console.WriteLine(week[i]);
+            }
+        }
+    }
+}
