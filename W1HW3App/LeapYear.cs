8768f818d0387fb74c3dea3028d9c91638852be7
minato aqua <homosapien@foxmail.com>
2022-02-28 18:50:15 +0800

    week1 commit

---
+++ W1HW3App/LeapYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int year =int.Parse(Console.ReadLine());
+                Console.WriteLine((year%4==0)?((year%100==0&&year%400!=0)?"no":"yes"):"no");
+            }
+            
+        }
+    }
+}
